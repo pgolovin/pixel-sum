687a0716e2074d95122738f42e196703954c4ce9
pgolovin <pavel.golovinskiy@gmail.com>
2022-02-05 17:17:52 +0300

    PixelSum constructor limitations and basic tests

--- include/pixel_sum.h
+++ include/pixel_sum.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <vector>
 //----------------------------------------------------------------------------
 // Class for providing fast region queries from an 8-bit pixel buffer.
 // Note: all coordinates are *inclusive* and clamped internally to the borders
@@ -19,7 +19,7 @@ class PixelSum
 public:
 
     PixelSum(const unsigned char* buffer, int xWidth, int yHeight);
-    ~PixelSum(void);
+    ~PixelSum(void) {};
 
     PixelSum(const PixelSum&);
     PixelSum& operator= (const PixelSum&);
@@ -28,5 +28,15 @@ public:
     double getPixelAverage(int x0, int y0, int x1, int y1) const;
     int getNonZeroCount(int x0, int y0, int x1, int y1) const;
     double getNonZeroAverage(int x0, int y0, int x1, int y1) const;
+
+private:
+    // Since our field is 4096sq max and each cell is 1Byte long
+    // the max number that can be stored in integral image if all cells contains max available value
+    // is 2^12 (4096) *2^12 * 2^8 = 2^(12+12+8) = 2^32 so uint32_t should sutisfy our purpose.
+    // 
+    // Q: what if size restrictions are not 4096 per edge but bigger,
+    // A: uint32_t will not work here we should use wider types like uint64_t
+    //    the same situation if cell size of initial image is not a Byte but Word
+    std::vector<uint32_t> m_integralImage;
 };
 //EOF
--- sources/pixel_sum.cpp
+++ sources/pixel_sum.cpp
@@ -1,5 +1,21 @@
 #include "include/pixel_sum.h"
-#include <vector>
+#include <stdexcept>
 
+PixelSum::PixelSum(const unsigned char* buffer, int xWidth, int yHeight)
+{
+    // Choose exception here
+    // Reason: if data is invalid then object should not be constructed
+    //         according to the standard this can be acheived by throwing exception.
+    if (!buffer)
+    {
+        throw std::invalid_argument("Null image provided");
+    }
+    // well since no clear specification about sizes restrictions was provided
+    // i assume i can treat 0 as an invalid size
+    if (xWidth <= 0 || yHeight <= 0)
+    {
+        throw std::invalid_argument("Negative or Zero image sizes provided");
+    }
+}
 
 // EOF
